c196524f03c47fbce690d0ca3ba8d6bd7274fddd
competitioner <anon>
2026-04-20 13:53:55 -0400

    Initial Commit

---
+++ Dev_Project/Dev_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dev_ProjectApp.swift
+//  Dev_Project
+//
+//  Created by Shaanzay Iqbal on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Dev_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
