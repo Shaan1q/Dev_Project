f691ee422b165101b4c97962b3e6889f2414d4ff
competitioner <anon>
2026-04-20 13:55:56 -0400

    Changed intro text and added a new folder

---
+++ Dev_Project/Sample.swift
@@ -0,0 +1,12 @@
+//
+//  Sample.swift
+//  Dev_Project
+//
+//  Created by Shaanzay Iqbal on 4/20/26.
+//
+import SwiftUI
+struct Sample: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
